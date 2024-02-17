b2fd488a60c2bf5548a2b067e3e8b57ade9caf19
Nisanth Selvaraj <121391945+Nisanth2004@users.noreply.github.com>
2024-02-17 11:56:59 +0530

    simple project

---
+++ guess-number-project/src/Result.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Result({secretNum,term})
+{
+    let result;
+    if(term){
+    if(term>secretNum)
+    {
+        result='Higher';
+    }
+    else if(term<secretNum)
+    {
+        result='Lower';
+    }
+
+    else if(term==secretNum)
+    {
+        result='Yipee!Correct';
+    }
+    else{
+        result='Enter valid input';
+    }
+}
+    return(
+    <>
+    <h3>You guessed: {result}</h3>
+    
+    </>
+    );
+}
+export default Result;
